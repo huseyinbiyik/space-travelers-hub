0f8e217228c5e83dc852695cc2f3840a5fb07463
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-06-28 17:41:11 +0300

    Add rocketsreducer and fetchrockets method

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,7 +1,25 @@
-import React from 'react';
+const apiUrl = 'https://api.spacexdata.com/v3/rockets';
 
-export default function rocketsReducer() {
-  return (
-    <div>rockets</div>
-  );
+const SHOW_ROCKETS = 'space-travelers-hub/rockets/rockets';
+
+export const showMissions = (data) => ({
+  type: SHOW_ROCKETS,
+  data,
+});
+
+export const fetchRockets = () => (
+  (dispatch) => {
+    fetch(`${apiUrl}`)
+      .then((response) => response.json())
+      .then((json) => dispatch(showMissions(json)));
+  }
+);
+
+export default function rocketsReducer(state = [], action) {
+  switch (action.type) {
+    case SHOW_ROCKETS:
+      return action.data;
+    default:
+      return state;
+  }
 }
