2fd096472a9c5d2649686f1d40230922e430674b
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-06-28 15:45:51 +0300

    Add header and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+} from 'react-router-dom';
 import Header from './components/Header';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <Header />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
