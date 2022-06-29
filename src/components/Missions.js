e3e34634b50dcfd76c096804a4fc6cf043bd35f2
Kwentsir <kwentsirkwesi@gmail.com>
2022-06-29 00:15:20 +0000

    Add missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchMissions } from "../redux/missions/missions";
 
 export default function Missions() {
-  return (
-    <div>Missions</div>
-  );
+  const data = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (data.length === 0) {
+      dispatch(fetchMissions());
+    }
+  }, []);
+  return <div>missions</div>;
 }
