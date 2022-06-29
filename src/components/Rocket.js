6710f855184ea032f310d31c2b6be0c1f7804890
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-06-29 07:41:31 +0300

    Add single rocket component

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-export default function Rocket() {
+export default function Rocket(props) {
+  const {
+    title, description, img, id,
+  } = props;
+
   return (
-    <div>Rocket</div>
+    <div className="single-rocket">
+      <div className="rocket-image-container">
+        <img src={img} alt={title} />
+      </div>
+      <div className="rocket-desc-container">
+        <h2 className="rocket-title">{title}</h2>
+        <p className="rocket-desc">{description}</p>
+        <button className="rocket-reserve-btn" id={id} type="submit">Reserve Rocket</button>
+      </div>
+    </div>
   );
 }
