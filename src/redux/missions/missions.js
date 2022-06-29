0821fae21c6fc0d25fc8e405ba38a31608fd244e
Kwentsir <kwentsirkwesi@gmail.com>
2022-06-29 00:02:26 +0000

    Add function to filter Missions

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -6,3 +6,13 @@ export const showMissions = (data) => ({
   type: SHOW_MISSIONS,
   data,
 });
+
+const filterMissions = (data) => {
+  const missions = data.map((mission) => ({
+    id: mission.id,
+    mission_name: mission.mission_name,
+    description: mission.description,
+    img: mission.flickr_images[0],
+  }));
+  return showMissions(missions);
+};
