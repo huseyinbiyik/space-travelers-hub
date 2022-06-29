68d0cd32f01cea47c94ee0feaae68127a9b9ec0d
Kwentsir <kwentsirkwesi@gmail.com>
2022-06-29 00:00:49 +0000

    Add function to show missions

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,3 +1,8 @@
 const missonsApiUrl = "https://api.spacexdata.com/v3/missions";
 
 const SHOW_MISSIONS = "space-travelers-hub/missions/missions";
+
+export const showMissions = (data) => ({
+  type: SHOW_MISSIONS,
+  data,
+});
