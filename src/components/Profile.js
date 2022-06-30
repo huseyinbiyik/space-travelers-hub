b939835c4e1faa025b9b9474b7645d2dc756c20a
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-06-30 16:11:56 +0300

    Add rocket map function

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,12 +3,21 @@ import './styles/Profile.css';
 import { useSelector } from 'react-redux';
 
 export default function Profile() {
-  const profileRocketData = useSelector((state) => state.rockets);
-  console.log(profileRocketData);
+  let profileRocketData = useSelector((state) => state.rockets);
+
+  profileRocketData = profileRocketData.filter((rocket) => (rocket.reserved));
+
   return (
     <div className="main-wrapper profile-wrapper">
       <div className="my-missions-wrapper"><h2>My Missions</h2></div>
-      <div className="my-rockets-wrapper"><h2>My Rockets</h2></div>
+      <div className="my-rockets-wrapper">
+        <h2>My Rockets</h2>
+        <div className="reserved-rockets-container">
+          {
+              profileRocketData.map((rocket) => (<p key={rocket.id}>{rocket.rocket_name}</p>))
+        }
+        </div>
+      </div>
     </div>
   );
 }
