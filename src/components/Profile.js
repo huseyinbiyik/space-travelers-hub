e4ba9ba9ea9807f3347211e6f307516efceeb958
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-06-30 15:16:02 +0300

    Add profile wrapper and styling file

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './styles/Profile.css';
+import { useSelector } from 'react-redux';
 
 export default function Profile() {
+  const profileRocketData = useSelector((state) => state.rockets);
+  console.log(profileRocketData);
   return (
-    <div>Profile</div>
+    <div className="main-wrapper profile-wrapper">
+      <div className="my-missions-wrapper"><h2>My Missions</h2></div>
+      <div className="my-rockets-wrapper"><h2>My Rockets</h2></div>
+    </div>
   );
 }
