e5ef68a0e604d55f10af254cf0595dd7dfdf9891
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-06-30 20:09:17 +0300

    Add unit tests for rocket components

--- src/tests/Rockets.test.js
+++ src/tests/Rockets.test.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 import store from '../Redux/configureStore';
-import Rockets from '../components/Rockets'
-import Rocket from '../components/Rocket'
-import showRockets from '../redux/rockets/rockets'
+import Rockets from '../components/Rockets';
+import Rocket from '../components/Rocket';
+import rocketsReducer, { showRockets, addRocketReservation, cancelRocketReservation } from '../redux/rockets/rockets';
+import rocketTestData from './testdata';
 
 describe('Rocket', () => {
   it('should render rocket component', () => {
@@ -25,4 +26,21 @@ describe('Rocket', () => {
     expect(rockets).toMatchSnapshot();
   });
 
-})
+  it('Should update the state with API data', () => {
+    expect(rocketsReducer(rocketTestData, showRockets(rocketTestData)))
+
+      .toEqual(rocketTestData);
+  });
+
+  it('Should update the state with correct id to reserved: true', () => {
+    const rocketId = '1';
+    expect(rocketsReducer(rocketTestData, addRocketReservation(rocketId)))
+      .toEqual(rocketTestData.map((d) => (d.id === rocketId ? { ...d, reserved: true } : d)));
+  });
+
+  it('Should update the state with correct id to reserved: false', () => {
+    const rocketId = '5';
+    expect(rocketsReducer(rocketTestData, cancelRocketReservation(rocketId)))
+      .toEqual(rocketTestData.map((d) => (d.id === rocketId ? { ...d, reserved: false } : d)));
+  });
+});
